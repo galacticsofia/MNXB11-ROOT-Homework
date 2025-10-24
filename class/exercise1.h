a9ea92bc378fb3558ca2e77ae811b3e9a2246e28
Sofia Slaniceanu <galacticsofia@gmail.com>
2025-10-24 22:04:01 +0200

    worked more on 1,2 and 3

--- class/exercise1.h
+++ class/exercise1.h
@@ -2,15 +2,26 @@
 #define EXERCISE1_H
 
 #include <TObject.h>
+#include <cmath>
 
 class Ex1class: public TObject{
     public:
         Ex1class();
-        Ex1class(Int_t x);
+        Ex1class(Double_t px, Double_t py, Double_t pz);
         virtual ~Ex1class();
 
+        Double_t pxEntry() const {return px;}
+        Double_t pyEntry() const {return py;}
+        Double_t pzEntry() const {return pz;}
+        Double_t pMag() const {return pmag;}
+
+ 
     private:
-        Int_t ex1var;
+        Double_t px;
+        Double_t py;
+        Double_t pz;
+
+        Double_t pmag;
     
     ClassDef(Ex1class, 1);
 };
--- class/write.cxx
+++ class/write.cxx
@@ -3,27 +3,28 @@
 #include "TTree.h"
 #include "TRandom.h"
 #include <iostream>
+#include <cmath>
+
 
 void write(){
 
-    Ex1class *object = nullptr;
+    Ex1class *instmomentum = nullptr;
 
-    TFile *file = new TFile("momentum.root");
-    TTree *tree = new TTree("tree", "TOjbect");
+    TFile *file = new TFile("tree_file.root", "RECREATE");
+    TTree *tree = new TTree("tree", "exercise tree");
 
-    tree->Branch("momentum_tree", "Ex1class", &object);
+    tree->Branch("momentum_branch", "Ex1class", &instmomentum);
 
-    Int_t datapoints = 500;
-    for (Int_t i = 0; i < datapoints; i++){
-        object = new Ex1class(i);
-        float px = gRandom->Gaus(0, .02);
-        float py = gRandom->Gaus(0, .02);
-        float pz = gRandom->Gaus(0, .02);
+    Int_t instances = 500;
+    for (Int_t i = 0; i < instances; i++){
+        
+        Double_t px = gRandom->Gaus(0, .02);
+        Double_t py = gRandom->Gaus(0, .02);
+        Double_t pz = gRandom->Gaus(0, .02);
 
+        instmomentum = new Ex1class(px, py, pz);
         tree->Fill();
     }
-    
     tree->Write();
     file->Close();
-    delete object;
 }
